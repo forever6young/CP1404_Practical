9136e6fece57f4273e0c38c4f255d79e6f684f05
wangzhaoyang <zhaoyang.wang1@my.jcu.edu.au>
2022-04-16 16:11:40 +0800

    Get words' frequency in text

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,1 +1,30 @@
-print()
+def main():
+    # Setup dict
+    my_dict = {}
+
+    text = input('Text: ')
+
+    # Remove white space
+    clean_text = ' '.join(text.split())
+
+    # Split data
+    raw_data = clean_text.split()
+
+    # Sort data
+    raw_data.sort()
+
+    for word in raw_data:
+        if word in my_dict:
+            my_dict[word] += 1
+        else:
+            my_dict[word] = 1
+
+    # Find longest word
+    longest_word_len = max([len(key) for key in my_dict])
+
+    # Print result
+    for key, value in my_dict.items():
+        print('{0:<{1}} : {2}'.format(key, longest_word_len, value))
+
+
+main()
