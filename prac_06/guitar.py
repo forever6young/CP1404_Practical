a1c53007f97f2b73c145e1dd259d8a1e736427f5
wangzhaoyang <zhaoyang.wang1@my.jcu.edu.au>
2022-05-03 20:37:53 +0800

    Initial commit

---
+++ prac_06/guitar.py
@@ -0,0 +1,19 @@
+current = 2022
+vintage = 50
+
+
+class Guitar:
+    def __init__(self, name="", year=0, cost=0):
+        """Initialise a Guitar."""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+    def get_age(self):
+        return current - self.year
+
+    def is_vintage(self):
+        return self.get_age() >= vintage
